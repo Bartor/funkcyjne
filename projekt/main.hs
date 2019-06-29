74d98baee08b06f5c1b9ccda1a8e00d5c3ce5554
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-06-29 19:33:43 +0200

    działa dla wielu plików i wyświetla staty

--- projekt/main.hs
+++ projekt/main.hs
@@ -6,19 +6,15 @@ import Data.List
 import Data.Function
 import Data.Typeable
 
--- uruchamiane jako "main.exe [liczba słów do wypisania] [plik ze stopami] [plik do analizy]"
--- potem będzie tam można wrzucić ich kilka
+-- uruchamiane jako "main.exe [liczba słów do wypisania] [plik ze stopami] [plik do analizy 1] [plik do analizy 2] ..."
+-- przykładowo .\main.exe 10 .\stop.words .\words.words .\words2.words
 main = do
     args <- getArgs
     let number = read $ head $ args :: Int
-    -- print $ typeOf number
-    -- print number
     stops <- readFile (head.tail $ args)
     let contents = map readFile (tail.tail $ args)
-    -- tak, nie znam się na typach w haskellu
-    -- print $ typeOf contents
-    -- print $ words stops
-    ((take number).frequency.(filterStop.words $ stops).clear.words <$> head contents) >>= print
+    let stats = map (\x -> ((take number).frequency.(filterStop.words $ stops).clear.words <$> x)) contents
+    mapM (\x -> x >>= print) stats
 
 clear :: [String] -> [String]
 clear s = filter (\x -> all (\y -> y `elem` ['a'..'z']++['A'..'Z']) x) s
