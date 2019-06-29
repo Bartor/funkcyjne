7c0f52a8eec43309154f345d6e16e79bc48deced
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-06-29 19:22:51 +0200

    postępy

--- projekt/main.hs
+++ projekt/main.hs
@@ -1,22 +1,24 @@
 import System.IO  
 import Control.Monad
+import System.Environment ( getArgs )
 import Data.Char
 import Data.List
 import Data.Function
+import Data.Typeable
 
+-- uruchamiane jako "main.exe [liczba słów do wypisania] [plik ze stopami] [plik do analizy]"
+-- potem będzie tam można wrzucić ich kilka
 main = do
-    -- tymczasowe wczytywanie z plików
-    stopHandle <- openFile "stop.words" ReadMode
-    stopWords <- hGetContents stopHandle
-
-    wordsHandle <- openFile "words.words" ReadMode
-    wordsWords <- hGetContents wordsHandle
-
-    -- na razie wypisuje 10 najpopularniejszych słów
-    print $ (take 10).frequency.(filterStop.words $ stopWords).clear.words $ wordsWords
-
-    hClose stopHandle
-    hClose wordsHandle
+    args <- getArgs
+    let number = read $ head $ args :: Int
+    -- print $ typeOf number
+    -- print number
+    stops <- readFile (head.tail $ args)
+    let contents = map readFile (tail.tail $ args)
+    -- tak, nie znam się na typach w haskellu
+    -- print $ typeOf contents
+    -- print $ words stops
+    ((take number).frequency.(filterStop.words $ stops).clear.words <$> head contents) >>= print
 
 clear :: [String] -> [String]
 clear s = filter (\x -> all (\y -> y `elem` ['a'..'z']++['A'..'Z']) x) s
@@ -26,3 +28,4 @@ filterStop w s = filter (\x -> not $ x `elem` w) s
 
 frequency :: [String] -> [(Int, String)]
 frequency s = sortBy (flip compare `on` fst) $ map (\x -> (length x, head x)) $ group.sort $ s
+
