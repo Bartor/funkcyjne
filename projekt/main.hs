b3b2df582b47bf7bad872701b4d7c42600602926
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-06-30 16:24:48 +0200

    lepsze indeksy jaccarda

--- projekt/main.hs
+++ projekt/main.hs
@@ -5,6 +5,7 @@ import Data.Char
 import Data.List
 import Data.Function
 import Control.Arrow
+import Data.Typeable
 
 -- uruchamiane jako "main.exe [liczba słów do wypisania] [plik ze stopami] [plik do analizy 1] [plik do analizy 2] ..."
 -- przykładowo ./main.exe 10 ./data/stop.words ./data/lotr.txt ./data/KJB.txt
@@ -14,8 +15,8 @@ main = do
     stops <- readFile (head.tail $ args)
     let contents = map readFile (tail.tail $ args)
     let stats = map (\ x -> take number . frequency . (filterStop . words $ stops) . clear . words <$> x) contents
-    (jaccard <$> (stats !! 0) <*> (stats !! 1)) >>= print
-    mapM (>>= print) stats
+    mapM (>>= print) [jaccard <$> s1 <*> s2 | s1 <- stats, s2 <- stats]
+    -- mapM (>>= print) stats
 
 clear :: [String] -> [String]
 clear s = map (map toLower) $ filter (all (\ y -> y `elem` ['a' .. 'z'])) s
@@ -27,15 +28,15 @@ frequency :: [String] -> [(Int, String)]
 frequency s = sortBy (flip compare `on` fst) $ map (length Control.Arrow.&&& head) $ group.sort $ s
 
 jaccard :: [(Int, String)] -> [(Int, String)] -> Double
-jaccard stats1 stats2 = fromIntegral(statSum(map(\ x -> intersection (find (\ y -> snd y == snd x) stats2) x) stats1))/fromIntegral (statSum(map (\ x -> statUnion (find (\ y -> snd y == snd x) stats2) x) stats1))
+jaccard stats1 stats2 = fromIntegral (statSum(map(\ x -> intersection (find (\ y -> snd y == snd x) stats2) x) stats1))/fromIntegral (statSum(map (\ x -> statUnion (find (\ y -> snd y == snd x) stats2) x) stats1))
 
 statSum :: [(Int, String)] -> Int
 statSum = foldl (\ a (f, _) -> a + f) 0
 
 intersection :: Maybe (Int, String) -> (Int, String) -> (Int, String)
 intersection Nothing (num1, token1) = (0, token1)
-intersection (Just (num1, token1)) (num2, token2) = if num1 > num2 then (num2, token1) else (num1, token2)
+intersection (Just (num1, token1)) (num2, token2) = if num1 > num2 then (num2, token2) else (num1, token1)
 
 statUnion :: Maybe (Int, String) -> (Int, String) -> (Int, String)
 statUnion Nothing x = x
-statUnion (Just (num1, token1)) (num2, token2) = (num1 + num2, token1)
+statUnion (Just (num1, token1)) (num2, token2) = if num1 > num2 then (num1, token1) else (num2, token2)
